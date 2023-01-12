1ca2fc7a4b9d87991cd203c2fe685d09d59d1984
Elijah Cruz <ecwebservices@gmail.com>
2023-01-11 21:49:54 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Larakit
+
+Larakit (Or Laravel Development Kit) is a Command Line tool that makes settings your Laravel project, or installing specific development tools (Like the Laravel-Debugbar), simple and easy.
+
+## Installation
+
+Installing Larakit is as simple as a global require:
+
+```
+    composer global require elijahcruz/larakit
+```
+
+# Usage
+
+## Installing Dev Packages
+
+You can install packages like the Debugbar or Laravel-Ide-Helper simply be running the `install` command:
+
+```
+    larakit install <item>
+```
+
+If you want to view the list of items you can install with the above command, run the `install:list` command:
+
+```
+    larakit install:list
+```
---
+++ app/Commands/InstallListCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class InstallListCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'install:list';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Lists all the items you can install via the "install" command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $composer_items = [
+            ['debugbar', 'barryvdh/laravel-debugbar'],
+            ['idehelper', 'barryvdh/laravel-ide-helper'],
+            ['ide-helper', 'barryvdh/laravel-ide-helper'],
+        ];
+
+        $this->info('Composer Installed Items');
+
+        $this->table(['Command Name', 'Package Name'], $composer_items);
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
